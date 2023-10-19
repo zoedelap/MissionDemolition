8f3aeaab9cc8ff178cb5ff01c9c8576552db50ad
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-10-19 11:51:48 -0500

    base slingshot script

---
+++ MissionDemolition/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    private void OnMouseEnter()
+    {
+        print("Slingshot:OnMouseEnter()");
+    }
+
+    private void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+    }
+}
