20ee87292ab29c77d2e7ae6ae3eb9c5a0ef00ef9
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-10-19 17:08:12 -0500

    fixed names

--- MissionDemolition/Assets/_Scripts/FollowCam.cs
+++ MissionDemolition/Assets/_Scripts/FollowCam.cs
@@ -21,14 +21,29 @@ public class FollowCam : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (POI == null) return;
+        Vector3 destination = Vector3.zero;
 
-        Vector3 destination = POI.transform.position;
+        if (POI != null)
+        {
+            Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+            if ((poiRigid != null) && poiRigid.IsSleeping())
+            {
+                POI = null;
+            }
+        }
+
+        if (POI != null)
+        {
+            destination = POI.transform.position;
+        }
+
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
 
         destination.z = camZ;
         transform.position = destination;
+
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
