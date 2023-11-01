e99893e19005dd34615939703f886d6f50d76019
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-11-01 13:21:33 -0500

    add dimming to the lines

--- MissionDemolition/Assets/_Scripts/ProjectileLine.cs
+++ MissionDemolition/Assets/_Scripts/ProjectileLine.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class ProjectileLine : MonoBehaviour
 {
+    static List<ProjectileLine> PROJ_LINES = new List<ProjectileLine>();
+    private const float DIM_MULT = 0.75f;
     private LineRenderer _line;
 
     private bool _drawing = true;
@@ -18,6 +20,8 @@ public class ProjectileLine : MonoBehaviour
         _line.SetPosition(0, transform.position);
 
         _projectile = GetComponentInParent<Projectile>();
+
+        ADD_LINE(this);
     }
 
     void FixedUpdate()
@@ -37,5 +41,24 @@ public class ProjectileLine : MonoBehaviour
                 }
             }
         }
+    }
+
+    private void OnDestroy()
+    {
+        PROJ_LINES.Remove(this);
+    }
+
+    static void ADD_LINE(ProjectileLine newLine)
+    {
+        Color col;
+
+        foreach (ProjectileLine pl in PROJ_LINES)
+        {
+            col = pl._line.startColor;
+
+            col = col * DIM_MULT;
+            pl._line.startColor = pl._line.endColor = col;
+        }
+        PROJ_LINES.Add(newLine);
     }
 }
